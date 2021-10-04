aa76948e1899aaa1263562b4eab9c8ee3c898413
chen <1124173993@qq.com>
2021-10-04 11:59:59 +0800

    123

--- src/main/java/demo/controller/DateTimeController.java
+++ src/main/java/demo/controller/DateTimeController.java
@@ -1,9 +1,7 @@
 package demo.controller;
 
-import demo.response.DateTimeResponse;
 import demo.request.DateTimeRequest;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
+import demo.response.DateTimeResponse;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +13,6 @@ public class DateTimeController {
 
     @PostMapping(value = "/testDateTime")
     public DateTimeResponse testDateTime(@RequestBody DateTimeRequest dateTimeRequest, HttpServletRequest request) {
-        String header = request.getHeader("123");
-//        Cookie[] cookies = request.getCookies();
-//        Enumeration<String> parameterNames = request.getParameterNames();
         return new DateTimeResponse(dateTimeRequest.getLocalDateTime(),dateTimeRequest.getLocalDate());
     }
 }
